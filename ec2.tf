9dfd6086624cae1c2fc48a880f24648369cf6075
Ishtiaq Ahmad <ishtiaq.uaar55@gmail.com>
2025-03-27 23:48:55 +0500

    added prd code

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terra-aws-ec2-instance" {
   ami             = "ami-084568db4383264d4"
-  instance_type   = "t2.micro"
+  instance_type   = "t2.small"
   key_name        = aws_key_pair.terra-key-pair.key_name
   security_groups = [aws_security_group.terra_user_to_connect.name]
   tags = {
